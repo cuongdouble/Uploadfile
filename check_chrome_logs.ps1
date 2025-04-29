fdc4fc7467281247d5fdf73646d9a3a7180b1959
Liam Tran <49640387+cuongdouble@users.noreply.github.com>
2025-04-29 16:59:50 +0700

    Add files via upload

--- check_chrome_logs.ps1
+++ check_chrome_logs.ps1
@@ -1,19 +1,19 @@
 $logPath = "C:\Users\kioskuser\AppData\Local\Google\Chrome\User Data\chrome_debug.log"
-$lastPosterLine = $null
+$posterLines = @()
 
 if (Test-Path $logPath) {
-    $reader = [System.IO.StreamReader]::new($logPath)
+    $reader = [System.IO.StreamReader]::new($logPath, [System.Text.Encoding]::UTF8)
 
     while (($line = $reader.ReadLine()) -ne $null) {
-        if ($line -match "poster_impression") {
-            $lastPosterLine = $line
+        if ($line -like "*poster_impression*") {
+            $posterLines += $line
         }
     }
 
     $reader.Close()
 
-    if ($lastPosterLine) {
-        Write-Output "Last line containing 'poster_impression':"
+    if ($posterLines.Count -gt 0) {
+        $lastPosterLine = $posterLines[-1]
         Write-Output $lastPosterLine
     } else {
         Write-Output "No line containing 'poster_impression' found."
